6253b3226b6635993089aa8edfacb1b91d0a1417
EdonJung <2donny@naver.com>
2021-07-23 14:56:12 +0900

    helmet provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React, { useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import MeetingRoom from "./pages/MeetingRoom";
-import { routes } from "./routes/index";
+import React, { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import MeetingRoom from './pages/MeetingRoom';
+import { routes } from './routes/index';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <Switch>
-      <Route path={routes.home} exact>
-        <Home />
-      </Route>
-      <Route path={routes.meetingRoom}>
-        <MeetingRoom />
-      </Route>
-    </Switch>
+    <HelmetProvider>
+      <Switch>
+        <Route path={routes.home} exact>
+          <Home />
+        </Route>
+        <Route path={routes.meetingRoom}>
+          <MeetingRoom />
+        </Route>
+      </Switch>
+    </HelmetProvider>
   );
 }
 
