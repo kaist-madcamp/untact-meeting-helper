9aa19a3ec5e344abc34525d11f623ec7ce3f6837
EdonJung <2donny@naver.com>
2021-07-28 16:55:51 +0900

    Chat box container

--- client/src/components/chat/ChatContainer.tsx
+++ client/src/components/chat/ChatContainer.tsx
@@ -7,12 +7,29 @@ export const ChatContainer = styled.div`
   height: 100%;
   z-index: 999;
   border-radius: 10px;
+  overflow: hidden;
 `;
 
 export const ChatMainBox = styled.div`
   width: 100%;
+  height: 100%;
+  overflow-x: hidden;
   overflow-y: scroll;
+  overscroll-behavior-y: contain;
+  scroll-snap-type: y proximity;
   padding: 15px;
+  padding-bottom: 100px;
+  scroll-snap-align: end;
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #aaa;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    margin-bottom: 100px;
+  }
 `;
 
 export const ChatBlock = styled.div<{ location: string }>`
@@ -20,7 +37,7 @@ export const ChatBlock = styled.div<{ location: string }>`
   justify-content: ${(props) =>
     props.location === 'left' ? 'flex-start' : 'flex-end'};
   & + & {
-    margin: 4px 0;
+    margin: 6px 0;
   }
 `;
 
@@ -30,6 +47,7 @@ export const ChatBox = styled.div`
   padding: 10px;
   font-size: 11px;
   color: #fff;
+  box-shadow: 0 3rem 10rem hsl(0, 0%, 60%);
   &.receive_box {
     background-color: #343434;
   }
